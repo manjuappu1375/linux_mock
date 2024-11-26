e06415431991318f3598c2f39881d41897fdab6a
manju <manjuappu1375@gmail.com>
2024-11-26 05:27:13 +0000

    added inrto.sh in branch1

---
+++ inrto.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Please enter your name:"
+read name
+
+echo "Please enter your profession/role:"
+read profession
+
+echo "Please enter your hobbies or interests:"
+read hobbies
+
+echo "Please enter what you are currently learning:"
+read learning
+
+echo ""
+echo "Hello! My name is $name."
+echo "I am a $profession."
+echo "I enjoy $hobbies."
+echo "I am currently learning $learning."
+echo "Thank you for taking the time to read my introduction!"
